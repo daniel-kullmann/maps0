2ef974b1adb55f1649a4ca64cbeea58a237259b7
Daniel Kullmann <daniel.kullmann@gmx.de>
2018-12-03 10:22:54 +0000

    tiles: handle errors, return 404

--- maps_backend/tiles/views.py
+++ maps_backend/tiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 import http.client
 import os
@@ -17,21 +17,24 @@ def get_tile(request,s,z,x,y):
             response['Access-Control-Allow-Origin'] = '*'
             return response
 
-    global context
-    if context == None:
-        context = ssl.create_default_context()
-    connection = http.client.HTTPSConnection(s + ".tile.openstreetmap.org", context=context)
-    connection.request('GET', "/"+z+"/"+x+"/"+y+".png")
-    response = connection.getresponse()
-    if response.status != 200:
-        return None # TODO
-    png = response.read()
-    if len(png) > 0:
-        os.makedirs(dirname, exist_ok=True)
-        with open(filename, "wb") as fh:
-            fh.write(png)
-    # TODO: read headers like e-tag and max-cache-age, and store them together
-    # with the file, along with info like last-update-date-time
-    response = HttpResponse(png, content_type='image/png')
-    response['Access-Control-Allow-Origin'] = '*'
-    return response
+    try:
+        global context
+        if context == None:
+            context = ssl.create_default_context()
+        connection = http.client.HTTPSConnection(s + ".tile.openstreetmap.org", context=context)
+        connection.request('GET', "/"+z+"/"+x+"/"+y+".png")
+        response = connection.getresponse()
+        if response.status != 200:
+            raise Http404()
+        png = response.read()
+        if len(png) > 0:
+            os.makedirs(dirname, exist_ok=True)
+            with open(filename, "wb") as fh:
+                fh.write(png)
+        # TODO: read headers like e-tag and max-cache-age, and store them together
+        # with the file, along with info like last-update-date-time
+        response = HttpResponse(png, content_type='image/png')
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+    except:
+        raise Http404()
