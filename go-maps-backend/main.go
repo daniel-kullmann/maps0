cfafa9e07dd79df78c88e750d233835bddade8ec
Daniel Kullmann <daniel.kullmann@gmx.de>
2018-12-16 00:01:19 +0000

    Bugfix: request to OSM failed because I didn't set the correct UserAgent. Also better error handling

--- go-maps-backend/main.go
+++ go-maps-backend/main.go
@@ -24,19 +24,37 @@ func GetTile(w http.ResponseWriter, r *http.Request) {
 	fileName := TileBase + strings.Join(parts, "/")
 	if _, err := os.Stat(fileName); os.IsNotExist(err) {
 
+		log.Print(fileName + ": fetch from OSM")
 		url := fmt.Sprintf("https://%s.tile.openstreetmap.org/%s/%s/%s", parts[0], parts[1], parts[2], parts[3])
-		client := http.DefaultClient
-		response, err := client.Get(url)
+		ua := r.Header.Get("User-Agent")
+		client := http.Client{}
+		request, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			w.Header().Add("Content-Type", "text/plain")
+			w.WriteHeader(404)
+			//w.Write(err.String())
+		}
+		request.Header.Set("User-Agent", ua)
+		response, err := client.Do(request)
 		if err != nil {
 			w.Header().Add("Content-Type", "text/plain")
 			w.WriteHeader(404)
 			response.Body.Close()
-			log.Panic(err)
+			log.Print("ERROR: Can't make request to " + url)
+		} else if response.StatusCode != 200 {
+			w.Header().Add("Content-Type", "text/plain")
+			w.WriteHeader(404)
+			w.Write([]byte(response.Status))
+			response.Body.Close()
+			log.Print("ERROR: Request resulted in " + string(response.StatusCode) + ", " + url)
 		} else {
 			w.Header().Add("Content-Type", "image/png")
 			w.Header().Add("Access-Control-Allow-Origin", "*")
-			os.MkdirAll(dirName, 0666)
+			os.MkdirAll(dirName, 0777)
 			fh, err_fh := os.Create(fileName)
+			if err_fh != nil {
+				log.Print("Can't create output file " + fileName)
+			}
 			buffer := make([]uint8, 100*1024)
 			for count, err := response.Body.Read(buffer); err == nil && count != 0; count, err = response.Body.Read(buffer) {
 				w.Write(buffer[0:count])
@@ -50,12 +68,12 @@ func GetTile(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.Header().Add("Content-Type", "image/png")
 		w.Header().Add("Access-Control-Allow-Origin", "*")
-		log.Print(fileName + " does exist")
+		log.Print(fileName + ": locally cached")
 		fh, err := os.Open(fileName)
 		if err != nil {
 			w.Header().Add("Content-Type", "text/plain")
 			w.WriteHeader(404)
-			log.Panic(err)
+			log.Print("ERROR: Can't read file " + fileName)
 		}
 		buffer := make([]uint8, 100*1024)
 		for count, err := fh.Read(buffer); err == nil && count != 0; count, err = fh.Read(buffer) {
